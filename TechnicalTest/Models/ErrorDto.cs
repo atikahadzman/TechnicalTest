cd6996dd8d3426fe2ba0e3469c5a07344c380ae0
atikahadzman <atikah.adzman@gmail.com>
2026-06-17 02:16:28 +0800

    allow nullable for stackTrace and innerException

--- TechnicalTest/Models/ErrorDto.cs
+++ TechnicalTest/Models/ErrorDto.cs
@@ -3,8 +3,8 @@ namespace TechnicalTest.Models;
 public class ErrorDto
 {
     public string message { get; set; } = "";
-    public string stackTrace { get; set; } = "";
-    public string innerException { get; set; } = "";
+    public string? stackTrace { get; set; } = "";
+    public string? innerException { get; set; } = "";
     public string columnName { get; set; } = "";
     public string columnValue { get; set; } = "";
 } 
