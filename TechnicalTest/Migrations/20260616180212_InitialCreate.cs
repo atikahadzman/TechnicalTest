f6566e80b81d41b8efce61cada7f1e462e2b4648
atikahadzman <atikah.adzman@gmail.com>
2026-06-17 02:24:44 +0800

    db migration

---
+++ TechnicalTest/Migrations/20260616180212_InitialCreate.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechnicalTest.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PlatformDummy",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false),
+                    uniqueName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    latitude = table.Column<double>(type: "float", nullable: false),
+                    longitude = table.Column<double>(type: "float", nullable: false),
+                    lastUpdate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlatformDummy", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Platforms",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    uniqueName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    latitude = table.Column<double>(type: "float", nullable: false),
+                    longitude = table.Column<double>(type: "float", nullable: false),
+                    createdAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    updatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Platforms", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WellDummy",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false),
+                    platformId = table.Column<int>(type: "int", nullable: false),
+                    uniqueName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    latitude = table.Column<double>(type: "float", nullable: false),
+                    longitude = table.Column<double>(type: "float", nullable: false),
+                    lastUpdate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WellDummy", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_WellDummy_PlatformDummy_platformId",
+                        column: x => x.platformId,
+                        principalTable: "PlatformDummy",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Wells",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    platformId = table.Column<int>(type: "int", nullable: false),
+                    uniqueName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    latitude = table.Column<double>(type: "float", nullable: false),
+                    longitude = table.Column<double>(type: "float", nullable: false),
+                    createdAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    updatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Wells", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Wells_Platforms_platformId",
+                        column: x => x.platformId,
+                        principalTable: "Platforms",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WellDummy_platformId",
+                table: "WellDummy",
+                column: "platformId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Wells_platformId",
+                table: "Wells",
+                column: "platformId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WellDummy");
+
+            migrationBuilder.DropTable(
+                name: "Wells");
+
+            migrationBuilder.DropTable(
+                name: "PlatformDummy");
+
+            migrationBuilder.DropTable(
+                name: "Platforms");
+        }
+    }
+}
