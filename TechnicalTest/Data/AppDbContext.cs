0295b18699962c2e047effe3c8ae813777868725
atikahadzman <atikah.adzman@gmail.com>
2026-06-17 02:12:17 +0800

    refactor code for database configuration

--- TechnicalTest/Data/AppDbContext.cs
+++ TechnicalTest/Data/AppDbContext.cs
@@ -10,12 +10,36 @@ public class AppDbContext : DbContext
 
     public DbSet<PlatformDto> Platforms => Set<PlatformDto>();
     public DbSet<WellDto> Wells => Set<WellDto>();
+    public DbSet<PlatformDummyDto> PlatformDummy => Set<PlatformDummyDto>();
+    public DbSet<WellDummyDto> WellDummy => Set<WellDummyDto>();
+
+    // protected override void OnModelCreating(ModelBuilder modelBuilder)
+    // {
+    //     modelBuilder.Entity<PlatformDto>()
+    //         .HasKey(x => x.id);
+
+
+    //     modelBuilder.Entity<WellDto>()
+    //         .HasKey(x => x.id);
+
+
+    //     modelBuilder.Entity<PlatformDto>()
+    //         .HasMany(x => x.Wells)
+    //         .WithOne()
+    //         .HasForeignKey(x => x.platformId);
+    // }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<PlatformDto>()
-            .HasMany(p => p.Wells)
-            .WithOne(w => w.Platform)
-            .HasForeignKey(w => w.platformId);
-    }
+        modelBuilder.Entity<PlatformDummyDto>()
+            .HasKey(x => x.id);
+
+        modelBuilder.Entity<WellDummyDto>()
+            .HasKey(x => x.id);
+
+        modelBuilder.Entity<PlatformDummyDto>()
+            .HasMany(x => x.Wells)
+            .WithOne(x => x.Platform)
+            .HasForeignKey(x => x.platformId);
+        }
 }
--- TechnicalTest/Program.cs
+++ TechnicalTest/Program.cs
@@ -16,7 +16,15 @@ builder.Services.AddHttpClient();
 
 // EF Core + SQL Server
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+{
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        sql =>
+        {
+            sql.EnableRetryOnFailure();
+        });
+});
+
 
 builder.Services.Configure<ApiSettings>(
 builder.Configuration.GetSection("ApiSettings"));
